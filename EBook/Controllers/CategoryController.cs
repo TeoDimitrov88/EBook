d07016adbffbbc698e30c4a76f52a4ef9027c7e1
Teo.DiMiTRov <don_teo_01@abv.bg>
2023-06-16 18:36:36 +0300

    Add project files.

---
+++ EBook/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using EBook.Data;
+using EBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> CategoryList= _context.Categories;
+            return View(CategoryList);
+        }
+    }
+}
