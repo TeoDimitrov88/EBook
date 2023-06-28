aa9d85ec6333e73fedb244d4de6e0362f6b8dff9
Teo.DiMiTRov <don_teo_01@abv.bg>
2023-06-28 23:46:44 +0300

    Adding CompanyId to AppUser and fix issues with register view

--- EBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,12 +11,14 @@ using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
 using EBook.DataAccess.Common;
+using EBook.Models.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualBasic;
@@ -102,6 +104,21 @@ namespace EBookWeb.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            public string? StreetAddress { get; set; }
+
+            public string? City { get; set; }
+
+            public string? State { get; set; }
+
+            public string? PostCode { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Role { get; set; }
+
+            public IEnumerable<SelectListItem>  RoleList { get; set; }
         }
 
 
@@ -110,31 +127,55 @@ namespace EBookWeb.Areas.Identity.Pages.Account
             if (!_roleManager.RoleExistsAsync(StaticConst.AdminRole).GetAwaiter().GetResult())
             {
 
-            _roleManager.CreateAsync(new IdentityRole(StaticConst.AdminRole)).GetAwaiter().GetResult();
-            _roleManager.CreateAsync(new IdentityRole(StaticConst.UserRole)).GetAwaiter().GetResult();
-            _roleManager.CreateAsync(new IdentityRole(StaticConst.CompanyRole)).GetAwaiter().GetResult();
-            _roleManager.CreateAsync(new IdentityRole(StaticConst.EmployeeRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticConst.AdminRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticConst.UserRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticConst.CompanyRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticConst.EmployeeRole)).GetAwaiter().GetResult();
             }
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem()
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
         }
 
         public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostCode = Input.PostCode;
+                user.Name = Input.Name;
+                user.PhoneNumber = Input.PhoneNumber;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    if (Input.Role== null)
+                    {
+                        await _userManager.AddToRoleAsync(user, StaticConst.UserRole);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user,Input.Role); 
+                    }
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
@@ -167,11 +208,11 @@ namespace EBookWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
