6435190f91cc3294ce1d1cd0f03ab59f7ec974b8
Teo.DiMiTRov <don_teo_01@abv.bg>
2023-07-05 00:09:21 +0300

    Adding view component for Shopping Cart

---
+++ EBook/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using EBook.DataAccess.Common;
+using EBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EBookWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim= claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(Constants.SessionCart) != null)
+                {
+                    return View(HttpContext.Session.GetInt32(Constants.SessionCart));
+                }
+                else
+                {
+                    HttpContext.Session.SetInt32(Constants.SessionCart, unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count);
+                    return View (HttpContext.Session.GetInt32(Constants.SessionCart));
+                }
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
