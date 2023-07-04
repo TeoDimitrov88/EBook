5f2a70da9ba3a3884079c324befdf9305ba59101
Teo.DiMiTRov <don_teo_01@abv.bg>
2023-07-04 15:25:33 +0300

    Cancel order fixed and fixed problem with views

--- EBook.DataAccess/Common/EmailSender.cs
+++ EBook.DataAccess/Common/EmailSender.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MailKit.Net.Smtp;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace EBook.DataAccess.Common
 {
     public class EmailSender : IEmailSender
     {
+       
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask;
+          return Task.CompletedTask;
         }
     }
 }
